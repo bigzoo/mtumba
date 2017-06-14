637466d90a615701d8d3a1634ab57a1764bcf271
Chris Nyaga <nyagachris75@gmail.com>
2017-06-14 16:22:07 +0300

    Products in the home page set to be only three

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.findAll('product');
+    return this.store.query('product',{
+      orderBy: 'cost',
+      limitToFirst: 3
+    });
   }
 });
+// model: function() {
+//     return this.store.query('task', {
+//       orderBy: 'done',
+//       equalTo: 'true',
+//       limitToFirst: 5
+//     });
+//   }
