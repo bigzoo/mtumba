c3adf27b8b0dab5457a83cf96fed197758672e2d
Chris Nyaga <nyagachris75@gmail.com>
2017-06-14 16:14:42 +0300

    Products are now displayed on a different page when the link is clicked and more information is available

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('product');
+  this.route('product', {path : '/products/:product_id'});
 });
 
 export default Router;
