4c74adfe2bdbbd20cde0b1d3639fe1892164786f
Chris Nyaga <nyagachris75@gmail.com>
2017-06-14 13:05:06 +0300

    Product attributes added to model

--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  name: DS.attr(),
+  img1: DS.attr(),
+  img2: DS.attr(),
+  img3: DS.attr(),
+  cost: DS.attr('number'),
+  description: DS.attr(),
+  availableStock: DS.attr()
 
 });
