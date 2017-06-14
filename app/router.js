d18c82836a1a8f9aabd9c57c7998c2d6805f7103
Chris Nyaga <nyagachris75@gmail.com>
2017-06-14 16:31:00 +0300

    New routes for about and contact added

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('product', {path : '/products/:product_id'});
   this.route('products');
+  this.route('about');
+  this.route('contact');
 });
 
 export default Router;
