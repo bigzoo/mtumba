becd5372d3043bd12ac82962c64cf2dd79c7892e
Chris Nyaga <nyagachris75@gmail.com>
2017-06-14 15:59:03 +0300

    Product model updated to include short description

--- app/models/product.js
+++ app/models/product.js
@@ -1,12 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  img1: DS.attr(),
-  img2: DS.attr(),
-  img3: DS.attr(),
+  name: DS.attr('string'),
+  img1: DS.attr('string'),
+  img2: DS.attr('string'),
+  img3: DS.attr('string'),
   cost: DS.attr('number'),
-  description: DS.attr(),
+  shortDescription: DS.attr('string'),
+  description: DS.attr('string'),
   availableStock: DS.attr('number')
 
 });
