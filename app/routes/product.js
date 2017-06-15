6961822e97304899f1337aee3b59ea3e2ce1340d
Chris Nyaga <nyagachris75@gmail.com>
2017-06-15 11:36:22 +0300

    Product tile created to hold product details

--- app/routes/product.js
+++ app/routes/product.js
@@ -2,16 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   shoppingCart: Ember.inject.service(),
-  inCart: false,
   model(params){
     return this.store.findRecord('product',params.product_id)
   },
   actions: {
     addToCart(item){
       this.get('shoppingCart').add(item);
-      console.log(this.get('inCart'));
-      this.set('inCart',true);
-      console.log(this.get('inCart'));
     },
     removeFromCart(item){
       this.get('shoppingCart').pop(item);
