7bcd5c19d07f24a348f362a7b968a0d081b4043d
Chris Nyaga <nyagachris75@gmail.com>
2017-06-15 14:59:24 +0300

    Checkout cart component included in checkout route and payment options added

--- app/components/checkout-cart.js
+++ app/components/checkout-cart.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+  // myItems: this.get('shoppingCart.items'),
+  actions:{
+    removeFromCart(item){
+      this.get('shoppingCart').pop(item);
+    }
+  }
 });
