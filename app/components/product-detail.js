ab297996e79cae575fc0109ae273a303af5de1c0
Chris Nyaga <nyagachris75@gmail.com>
2017-06-15 12:19:31 +0300

    You can now add and remove items from cart.

--- app/components/product-detail.js
+++ app/components/product-detail.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  outCart: true,
+  inCart: false,
   actions:{
     addToCart(item){
       this.sendAction('addToCart',item);
+      this.set('inCart',true);
+      this.set('outCart',false);
     },
     removeFromCart(item){
       this.sendAction('removeFromCart',item);
+      this.set('inCart',false);
+      this.set('outCart',true);
     }
   }
 });
